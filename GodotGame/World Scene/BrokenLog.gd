701a9e794fba3f7ad851913e14e343999ae8fff0
Thien <thienguyen134@gmail.com>
2024-03-03 22:44:14 -0600

    added gitignore, scenes for item pickup & 'well' to be upgraded. needs inventory check

---
+++ GodotGame/World Scene/BrokenLog.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@onready var dialogue_box = $DialogueBox
+@export var questReceived = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_body_entered(body):
+	if (body.name == "CharacterBody2D"):
+		print("Player has entered the tree stump")
+		if not dialogue_box.running:
+			dialogue_box.start()
+			
+
+func _on_body_exited(body):
+	if (body.name == "CharacterBody2D"):
+		print("Player has left the tree stump")
+		if dialogue_box.running:
+			dialogue_box.stop()
