74f7c862c8f06234c8922876fe0e2f6f621033bd
FrancisTR <fatran@stthomas.edu>
2024-03-08 00:32:21 -0600

    Added more functionality and exported the game for both Mac and Windows
    Former-commit-id: 824688720b35848bc9dbe277a53e34bc42537df7

--- GodotGame/World Scene/npc.gd
+++ GodotGame/World Scene/npc.gd
@@ -6,5 +6,14 @@ extends Area2D
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		print(true)
+		if (GameData.twigItem == 1):
+			dialogue_box.start_id = "NPC2"
 		if not dialogue_box.running:
 			dialogue_box.start()
+
+
+func _on_body_exited(body):
+	if (body.name == "CharacterBody2D"):
+		print("Player has left the tree stump")
+		if dialogue_box.running:
+			dialogue_box.stop()
