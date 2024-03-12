d3a9a9ea07bc124e76916803ed467037b9718ae0
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-03-12 18:19:56 -0500

    Script for buttons. Stub textbox. Widen ⌂ buttons.

--- GodotGame/Main Menu Scene/CanvasLayer.gd
+++ GodotGame/Main Menu Scene/CanvasLayer.gd
@@ -10,3 +10,7 @@ func _on_button_pressed():
 	print(line_edit.text)
 	GameData.username = line_edit.text
 	get_tree().change_scene_to_file("res://World Scene/World.tscn")
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://Main Menu Scene/MainMenu.tscn")
