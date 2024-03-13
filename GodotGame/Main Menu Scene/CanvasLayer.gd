66503ab7f8a0053415c1204b558773fe4782ff09
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-03-13 10:42:00 -0500

    Mv code to Options.gd + user validation

--- GodotGame/Main Menu Scene/CanvasLayer.gd
+++ GodotGame/Main Menu Scene/CanvasLayer.gd
@@ -11,6 +11,3 @@ func _on_button_pressed():
 	GameData.username = line_edit.text
 	get_tree().change_scene_to_file("res://World Scene/World.tscn")
 
-
-func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://Main Menu Scene/MainMenu.tscn")
