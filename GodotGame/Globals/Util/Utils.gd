49f67d22c7e0770b800f9798b2ce42c87f793aa1
inotp <tran6854@stthomas.edu>
2024-03-20 03:23:11 -0500

    implemented inventory data structure (array)
    Former-commit-id: d80240aeac2132527e9ea55a4e6d6696353b5473

---
+++ GodotGame/Globals/Util/Utils.gd
@@ -0,0 +1,46 @@
+extends Node
+
+const SAVE_PATH = "users://savegame.bin"
+
+func save_game():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	var data = {
+	}
+	
+	
+func load_game():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	
+func get_JSON(file_path):
+	var json_as_text = FileAccess.get_file_as_string(file_path)
+	var json_as_dict = JSON.parse_string(json_as_text)
+	return json_as_dict
+	
+	
+func get_item(item):
+	var test = get_JSON("res://Globals/items.json")
+	var index = test[0][item]
+	print(test[index])
+	return test[index]
+	
+func add_to_inventory(item, amount):
+	if GameData.inventory_amount.has(item):
+		GameData.inventory_amount[str(item)] = GameData.inventory_amount[str(item)] + amount
+	else:
+		GameData.inventory_amount[str(item)] = amount
+		GameData.inventory.append(get_item(item))
+	print("INVENTORY", GameData.inventory)
+	print("AMOUNT", GameData.inventory_amount)
+
+func remove_from_inventory(item, amount):
+	if(GameData.inventory_amount.has(item)):
+		if GameData.inventory_amount[str(item)] - amount <= 0:
+			GameData.inventory_amount[str(item)] = 0
+			if GameData.inventory.find(get_item(item)) != -1:
+				GameData.inventory.remove_at(GameData.inventory.find(get_item(item)))
+		else:
+			GameData.inventory_amount[str(item)] = GameData.inventory_amount[str(item)] - amount
+		print("INVENTORY", GameData.inventory)
+		print("AMOUNT", GameData.inventory_amount)
+	else:
+		print("No item to remove")
