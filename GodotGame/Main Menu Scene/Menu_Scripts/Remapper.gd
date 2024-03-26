1ce3a28ec93132153a5b74e177f587c0de897555
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-03-25 23:02:30 -0500

    Mappings no longer can be duplicated. See desc.
    Note: Physical keys and non-physical keys are treated as different keys.
    For example: A* and A. These work different in different keyboard
    layouts.
    
    Former-commit-id: 3b0c186633f5712682d91eabc9b5c281c14fb44f

--- GodotGame/Main Menu Scene/Menu_Scripts/Remapper.gd
+++ GodotGame/Main Menu Scene/Menu_Scripts/Remapper.gd
@@ -3,7 +3,9 @@ extends Button
 #TODO: Make user change the button one at a time
 
 @export var action: String
-@onready var remap_container = $"."
+#@onready var button = $"."
+@onready var remap_container = $".."
+
 #var not_waiting_input = false
 
 func _init():
@@ -14,39 +16,75 @@ func _ready():
 	set_process_unhandled_input(false)
 	update_text()
 	$HintLabel.text = action
+	#actions.append(action)
 
 func _toggled(button_pressed):
 	set_process_unhandled_input(button_pressed)
 	if button_pressed:
 		release_focus()
 		text = "???"
-		
-
 
 func _unhandled_input(e):
 	if not e is InputEventKey:
 		return
 	
 	if e.pressed:
-		InputMap.action_erase_events(action)
-		InputMap.action_add_event(action, e)
-		button_pressed = false
+		for k in get_all_keymaps():
+			#print(k)
+			#print(e)
+			#print("---")
+			if e.keycode == k.keycode:
+				# Move below message to in game
+				button_pressed = false
+				break
+		
+		if button_pressed:
+			button_pressed = false
+			InputMap.action_erase_events(action)
+			InputMap.action_add_event(action, e)
+		else:
+			print("Key is already used. Not setting new key.")
+
 		grab_focus()
 		update_text()
 
 func update_text():
-	var tmp = InputMap.action_get_events(action)[0].as_text()
+	text = get_keymap_name(action)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
+
+func get_all_keymaps() -> Array[InputEventKey]:
+	var children = remap_container.get_children()
+	var arr: Array[InputEventKey]
+	for child in children:
+		# below = wrong function. should get InputEvent instead of action or name
+		#arr.append(get_keymap_name(child.action))
+		arr.append(InputMap.action_get_events(child.action)[0])
+		
+		
+	print("size=" + str(children.size())) #is_empty
+	return arr
+	
+	
+func get_all_actions() -> Array[String]:
+	var children = remap_container.get_children()
+	var arr: Array[String]
+	for child in children:
+		arr.append(child.action)
+		
+	return arr
+
+func get_keymap_name(action_name: String) -> String:
+	var tmp = InputMap.action_get_events(action_name)[0].as_text()
 	tmp = tmp.split(" ")
 	var text_tmp: String
 	#if tmp[-1] == "(Physical)":
 	for word in tmp:
-		print(word)
+		#print(word)
 		if word != "(Physical)":
 			text_tmp = text_tmp + word
 		else:
 			text_tmp = text_tmp + "*"
 			
-	text = " " + text_tmp + " "
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#pass
+	return " " + text_tmp + " "
