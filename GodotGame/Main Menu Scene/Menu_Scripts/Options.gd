91b4e9aa1232636f22b463154af629db2ce06cfa
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-03-26 23:28:15 -0500

    Options doesn't go to home screen + improvements

--- GodotGame/Main Menu Scene/Menu_Scripts/Options.gd
+++ GodotGame/Main Menu Scene/Menu_Scripts/Options.gd
@@ -3,12 +3,17 @@ extends CanvasLayer
 @onready var line_edit = get_node("TextureRect/LineEdit")
 
 ## Called when the node enters the scene tree for the first time.
-#func _ready():
+func _ready():
 	#line_edit.grab_focus()
+	$BackButton.grab_focus()
 
 
 func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://Main Menu Scene/MainMenu.tscn")
+	if GameData.username:
+		get_tree().change_scene_to_file("res://World Scene/World.tscn")
+	else:
+		get_tree().change_scene_to_file("res://Main Menu Scene/MainMenu.tscn")
+	
 
 
 # Related script: VolumeOptions.gd
