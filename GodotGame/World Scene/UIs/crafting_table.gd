d850e83fd37f33cf7437cc01adc86f3f2c47b223
FrancisTR <fatran@stthomas.edu>
2024-04-03 01:58:30 -0500

    Added details to the world and other enhancements.

--- GodotGame/World Scene/UIs/crafting_table.gd
+++ GodotGame/World Scene/UIs/crafting_table.gd
@@ -1,34 +1,35 @@
 extends Node2D
 
+var enterBody = false
 
 # Called when the node enters the scene tree for the first time.
 var craftingList:Dictionary
 var ItemOfTheDay
 var itemImage
 @onready var listText = [
-	$CraftingList/ItemList/ItemBox/Item,
-	$CraftingList/ItemList/ItemBox2/Item2,
-	$CraftingList/ItemList/ItemBox3/Item3,
-	$CraftingList/ItemList/ItemBox4/Item4,
-	$CraftingList/ItemList/ItemBox5/Item5,
-	$CraftingList/ItemList/ItemBox6/Item6,
-	$CraftingList/ItemList/ItemBox7/Item7
+	$UI/CraftingList/ItemList/ItemBox/Item,
+	$UI/CraftingList/ItemList/ItemBox2/Item2,
+	$UI/CraftingList/ItemList/ItemBox3/Item3,
+	$UI/CraftingList/ItemList/ItemBox4/Item4
 ]
 @onready var listTextNumber = [
-	$CraftingList/ItemList/ItemBox/Item/ItemText, 
-	$CraftingList/ItemList/ItemBox2/Item2/Item2Text,
-	$CraftingList/ItemList/ItemBox3/Item3/Item3Text,
-	$CraftingList/ItemList/ItemBox4/Item4/Item4Text,
-	$CraftingList/ItemList/ItemBox5/Item5/Item5Text,
-	$CraftingList/ItemList/ItemBox6/Item6/Item6Text,
-	$CraftingList/ItemList/ItemBox7/Item7/Item7Text
+	$UI/CraftingList/ItemList/ItemBox/Item/ItemText, 
+	$UI/CraftingList/ItemList/ItemBox2/Item2/Item2Text,
+	$UI/CraftingList/ItemList/ItemBox3/Item3/Item3Text,
+	$UI/CraftingList/ItemList/ItemBox4/Item4/Item4Text
+]
+@onready var listTextImg = [
+	$UI/CraftingList/ItemList/ItemBox/ItemImg, 
+	$UI/CraftingList/ItemList/ItemBox2/ItemImg2,
+	$UI/CraftingList/ItemList/ItemBox3/ItemImg3,
+	$UI/CraftingList/ItemList/ItemBox4/ItemImg4
 ]
 var listKeys
 var listValues
 
 #TODO Add crafting recipes
 func _ready():
-	$CraftingList.visible = false
+	$UI/CraftingList.visible = false
 	if GameData.day == 2:
 		craftingList = {"Twig": 6, "TinCan": 1}
 		listKeys = craftingList.keys()
@@ -53,16 +54,28 @@ func _process(delta):
 		for i in range(0, len(craftingList)):
 			listText[i].text = "" + str(listKeys[i])
 			listTextNumber[i].text = "" + str(listValues[i])
+			#add textures
+			if listKeys[i] == "Twig":
+				listTextImg[i].texture = load("res://Assets/CraftingPlaceholder.png")
+			elif listKeys[i] == "TinCan":
+				listTextImg[i].texture = load("res://Assets/TinCan.png")
+			elif listKeys[i] == "WaterBottle":
+				listTextImg[i].texture = load("res://Assets/WaterBottle.png")
+			elif listKeys[i] == "Sand":
+				listTextImg[i].texture = load("res://Assets/Sand.png")
+			elif listKeys[i] == "Rock":
+				listTextImg[i].texture = load("res://Assets/Custom/temp_rock.png")
+			elif listKeys[i] == "Moss":
+				listTextImg[i].texture = load("res://Assets/Moss.png")
+				
 	else:
-		$CraftingList/CraftButton.visible = false
-	pass
-
-
-func _on_body_entered(body):
-	if (body.name == "CharacterBody2D"):
-		print("Entered crafting")
-		$CraftingList.visible = true
+		$UI/CraftingList/CraftButton.visible = false
 		
+		
+		
+	if Input.is_action_just_pressed("StartDialogue") and enterBody == true:	
+		$UI/CraftingList.visible = true
+		$PressInteraction.visible = false
 		#Enable the crafting button if met
 		listKeys = craftingList.keys()
 		listValues = craftingList.values()
@@ -71,17 +84,28 @@ func _on_body_entered(body):
 			if (GameData.inventory_amount.keys().find(listKeys[i]) != -1):
 				if GameData.inventory_amount[listKeys[i]] >= craftingList[listKeys[i]]:
 					count = count + 1
-		
+			
 		if count == len(listKeys):
-			$CraftingList/CraftButton.disabled = false
+			$UI/CraftingList/CraftButton.disabled = false
 		else:
-			$CraftingList/CraftButton.disabled = true
+			$UI/CraftingList/CraftButton.disabled = true
+	pass
+
+
+func _on_body_entered(body):
+	if (body.name == "CharacterBody2D"):
+		print("Entered crafting")
+		$PressInteraction.visible = true
+		$UI/CraftingList.visible = false
+		enterBody = true
 	pass # Replace with function body.
 
 
 func _on_body_exited(body):
 	if (body.name == "CharacterBody2D"):
-		$CraftingList.visible = false
+		$PressInteraction.visible = false
+		$UI/CraftingList.visible = false
+		enterBody = false
 		print("Exit crafting")
 	pass # Replace with function body.
 
@@ -94,19 +118,23 @@ func _on_craft_button_pressed():
 		for i in range(0, len(listKeys)):
 			Utils.remove_from_inventory(str(listKeys[i]), int(craftingList[listKeys[i]]))
 		Utils.add_to_inventory(str(ItemOfTheDay), 1)
-		$CraftingList.visible = false
+		$UI/CraftingList.visible = false
 		
-		$CraftedItem.visible = true
-		GameData.charLock = true
+		$UI/CraftedItem.visible = true
 		#TODO add what item the player made along with an image
-		$CraftedItem/ColorRect/ItemName.text = ItemOfTheDay
-		$CraftedItem/ColorRect/ItemImage.texture = itemImage
-
+		$UI/CraftedItem/ColorRect/ItemName.text = ItemOfTheDay
+		$UI/CraftedItem/ColorRect/ItemImage.texture = itemImage
 
 
 
 
 func _on_okay_button_pressed():
-	$CraftedItem.visible = false
+	$UI/CraftedItem.visible = false
+	GameData.charLock = false
+	pass # Replace with function body.
+
+
+func _on_x_button_pressed():
+	$UI/CraftingList.visible = false
 	GameData.charLock = false
 	pass # Replace with function body.
