7aff1fce2a39aa4fd16862efc2fece97ba2e64f1
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-04-09 13:18:28 -0500

    Fix volume options not saving + clean code
    Former-commit-id: e492cb6499117de1079ef9fa51955dd4c5c681be

--- GodotGame/Main Menu Scene/Menu_Scripts/VolumeOptions.gd
+++ GodotGame/Main Menu Scene/Menu_Scripts/VolumeOptions.gd
@@ -1,22 +1,18 @@
 extends HSlider
 @onready var vol_label = get_node("SFXVolumeValueLabel")
-var fix_vol_temp = false
 
 @export
-var bus_name: String # might be better to just have local variables
+var bus_name: String
 var bus_index: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
+
 	value_changed.connect(_on_value_changed)
-	value = db_to_linear(AudioServer.get_bus_index(bus_name))
+	value = get_volume()
 	
 func _on_value_changed(v: float) -> void:
-	if not fix_vol_temp:
-		fix_vol_temp = true
-		v = 100
-		$".".value = v
 	#print("[slider] bus index: ", bus_index)
 	
 	AudioServer.set_bus_volume_db(
@@ -24,5 +20,9 @@ func _on_value_changed(v: float) -> void:
 		linear_to_db(v / 100)
 	)
 	
-	vol_label.text = str(v) + "%"
+	vol_label.text = str(get_volume()) + "%"
 	#print(str(bus_index)+":"+str(v))
+
+func get_volume() -> int:
+	return int(db_to_linear(
+		AudioServer.get_bus_volume_db(bus_index)) * 100)
