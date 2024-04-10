98dc7e9cae4ff5999fde0d7a098325af6c40d48c
inotp <tran6854@stthomas.edu>
2024-04-09 19:13:24 -0500

    options menu 90% fixed

--- GodotGame/Main Menu Scene/Menu_Scripts/MainMenu.gd
+++ GodotGame/Main Menu Scene/Menu_Scripts/MainMenu.gd
@@ -97,7 +97,8 @@ func _on_start_button_pressed():
 
 func _on_option_button_pressed():
 	SoundControl.is_playing_sound("button")
-	SceneTransition.change_scene("res://Main Menu Scene/Options.tscn")
+	#SceneTransition.change_scene("res://Main Menu Scene/Options.tscn")
+	Utils.go_to_option_menu(get_tree().current_scene.scene_file_path)
 
 
 func _on_credits_pressed():
