fcec798a52cc7a65829b235cdb18664c87704da8
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-04-11 13:37:49 -0500

    Fixed focused buttons. Rewrote some `remapped.gd`

--- GodotGame/Main Menu Scene/Menu_Scripts/Remapper.gd
+++ GodotGame/Main Menu Scene/Menu_Scripts/Remapper.gd
@@ -1,10 +1,9 @@
 extends Button
 
-#TODO: Make user change the button one at a time
-
 @export var action: String
 #@onready var button = $"."
 @onready var remap_container = $".."
+@onready var remap_children := remap_container.get_children()
 
 #var not_waiting_input = false
 
@@ -18,37 +17,44 @@ func _ready():
 	$HintLabel.text = action
 	#actions.append(action)
 
-func _toggled(button_pressed):
-	set_process_unhandled_input(button_pressed)
-	if button_pressed:
+func _toggled(button_exists):
+	print("button_in_use == ", remap_container.button_in_use)
+
+	set_process_unhandled_input(button_exists)
+	if button_exists:
+		toggle_disabled_other_buttons()
 		release_focus()
 		text = "???"
 		SoundControl.is_playing_sound("button")
-	
-	
+
+
 func _unhandled_input(e):
-	if not e is InputEventKey:
+	toggle_disabled_other_buttons()
+
+	if not e is InputEventKey: 
 		return
-	
-	if e.pressed:
+
+	if e.pressed: # Check key that was just pressed
 		for k in get_all_keymaps():
 			#print(k)
 			#print(e)
 			#print("---")
 			if e.keycode == k.keycode:
 				# Move below message to in game
+				print("Key is already used. Not setting new key.")
 				button_pressed = false
+				#toggle_disabled_other_buttons()
 				break
-		
+
 		if button_pressed:
-			button_pressed = false
+			button_pressed = false # toggle button pressed
 			InputMap.action_erase_events(action)
 			InputMap.action_add_event(action, e)
-		else:
-			print("Key is already used. Not setting new key.")
+			#toggle_disabled_other_buttons()
 
 		grab_focus()
 		update_text()
+
 
 func update_text():
 	text = get_keymap_name(action)
@@ -57,40 +63,41 @@ func update_text():
 	#pass
 
 func get_all_keymaps() -> Array[InputEventKey]:
-	var children = remap_container.get_children()
 	var arr: Array[InputEventKey]
-	for child in children:
+	for child in remap_children:
 		# below = wrong function. should get InputEvent instead of action or name
 		#arr.append(get_keymap_name(child.action))
 		arr.append(InputMap.action_get_events(child.action)[0])
-		
-		
-	print("size=" + str(children.size())) #is_empty
+
+
+	print("size=" + str(remap_children.size())) #is_empty
 	return arr
-	
+
 	
 func get_all_actions() -> Array[String]:
 	var children = remap_container.get_children()
 	var arr: Array[String]
 	for child in children:
 		arr.append(child.action)
-		
+
 	return arr
 
 func get_keymap_name(action_name: String) -> String:
 	var tmp = InputMap.action_get_events(action_name)[0].as_text().split(" ")
 	
-	var text_tmp: String
-	var last_word: String
-	var last_i: int = tmp.size() - 1
-	
-	if tmp[last_i] == "(Physical)":
-		#tmp[last_i] = ""
+	if tmp.size() > 1:
 		tmp = tmp[0]
 	else:
-		#tmp[last_i] = tmp[last_i] + "*"
 		tmp = tmp[0] + "*"
 	
 	print(tmp)
-	#return " " + " ".join(tmp) + " "
 	return " " + tmp + " "
+
+func toggle_disabled_other_buttons():
+	remap_container.button_in_use = not remap_container.button_in_use
+	var is_disabled = remap_container.button_in_use
+	
+	#print("will be ", is_disabled)
+	for child in remap_children:
+		if child.name != name:
+			child.disabled = is_disabled
